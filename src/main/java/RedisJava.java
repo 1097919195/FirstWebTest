70bf5ae830a86e46ce7b556f5854b606df27000d
zjl7577 <1097919195@qq.com>
2018-09-15 17:59:35 +0800

    temp

--- src/main/java/RedisJava.java
+++ src/main/java/RedisJava.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisJava {
     public static void main(String[] args) {
+
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("127.0.0.1");
         System.out.println("连接成功");
